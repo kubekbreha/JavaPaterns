7badb33997c56ac9c4aa507b649847dd05885328
Jakub Brehuv <jakub.brehuv@student.tuke.sk>
2017-12-20 23:20:42 +0100

    add memento

--- behavioral/Memento/src/com/paterns/Main.java
+++ behavioral/Memento/src/com/paterns/Main.java
@@ -3,6 +3,23 @@ package com.paterns;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Originator originator = new Originator();
+        CareTaker careTaker = new CareTaker();
+
+        originator.setState("State #1");
+        originator.setState("State #2");
+        careTaker.add(originator.saveStateToMemento());
+
+        originator.setState("State #3");
+        careTaker.add(originator.saveStateToMemento());
+
+        originator.setState("State #4");
+        System.out.println("Current state: " + originator.getState());
+
+        originator.getStateFromMemento(careTaker.get(0));
+        System.out.println("First saved State: " + originator.getState());
+        originator.getStateFromMemento(careTaker.get(1));
+        System.out.println("Second saved State: " + originator.getState());
     }
 }
