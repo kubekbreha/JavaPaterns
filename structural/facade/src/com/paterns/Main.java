734c8264bd145fd1c83705c5accc8474a15a9555
Jakub Brehuv <jakub.brehuv@student.tuke.sk>
2017-12-20 09:14:35 +0100

    add decorate and facade

---
+++ structural/facade/src/com/paterns/Main.java
@@ -0,0 +1,12 @@
+package com.paterns;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ShapeMaker maker = new ShapeMaker();
+
+        maker.drawCircle();
+        maker.drawRectangle();
+        maker.drawSquare();
+    }
+}
