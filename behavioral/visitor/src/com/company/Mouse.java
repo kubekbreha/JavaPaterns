b2219d82b27b631c5720e81fff35347918d21f81
root <root@localhost.localdomain>
2017-12-21 13:51:15 -0500

    add visitor

---
+++ behavioral/visitor/src/com/company/Mouse.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Mouse implements ComputerPart {
+
+    @Override
+    public void accept(ComputerPartVisitor computerPartVisitor) {
+        computerPartVisitor.visit(this);
+    }
+}
