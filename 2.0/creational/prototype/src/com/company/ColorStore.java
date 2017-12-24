dc19df02f798dcf39f670db819118e5651a63352
Jakub Brehuv <kubek@kubek.grizzly>
2017-12-24 15:07:53 +0100

    prototype

---
+++ 2.0/creational/prototype/src/com/company/ColorStore.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ColorStore {
+    private static Map<String, Color> colorMap = new HashMap<String, Color>();
+
+    static
+    {
+        colorMap.put("blue", new blueColor());
+        colorMap.put("black", new blackColor());
+    }
+
+    public static Color getColor(String colorName)
+    {
+        return (Color) colorMap.get(colorName).clone();
+    }
+}
